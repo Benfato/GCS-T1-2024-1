74267c76c03d7c22e4773c35f548a3a524513a74
v-Kaefer <128633407+v-Kaefer@users.noreply.github.com>
2024-05-25 21:15:07 -0300

    Update Ingresso.java
    * Ajuste vendasIngressoDia
      - removido da função incrementaIngresso(), implementado na função getValorIngresso().
    
    * Ajuste getValorIngresso
      * registro do valor ddas vendas em vendasIngressoDia<>

--- Ingresso.java
+++ Ingresso.java
@@ -7,30 +7,79 @@ public class Ingresso {
     */
 
     private static int controleIngresso = 1;
-    private int ingresso;
+    private final int ingresso;
     private int valor;
     private atributosVisitante visitante;
     private final String identificador;
 
+    /* Considerar implementação de enum para validade do ingresso ou manter comparação de datas.
     enum validadeIngresso {
         INVALIDO, VALIDO
     }
+    */
 
-    public Ingresso(atributosVisitante visitante, String data) {
+    public Ingresso(atributosVisitante visitante, String data, int anoDeNascimento) {
         this.visitante = visitante;
         this.ingresso = incrementaIngresso(); // Usa a funcao incrementaIngresso(), atribuind valor a ingresso
-        this.identificador = subMenu.getData() + " - Ingresso: " + String.format("%03d", ingresso) + " - Valor: " + getValorIngresso(visitante.tipo);
-        this.valor = valor;
+        this.identificador = subMenu.getData() + " - Ingresso: " + String.format("%03d", ingresso) + " - Valor: " + getValorIngresso(anoDeNascimento);
+        this.valor = getValorIngresso(anoDeNascimento); // Por que eu adicionei isso?
     }
 
-    // Verificar a adição de ingressos na lista.
+
+    private Integer incrementaIngresso() {
+        if (controleIngresso < 500) {
+            ingresso = controleIngresso;
+            controleIngresso++;
+        } else if (controleIngresso < 500) {
+            ingresso = controleIngresso;
+            controleIngresso++;
+        } else if (controleIngresso == 500){
+            System.out.println("Erro: Limite de Ingressos diários atingido.");
+        }
+        // Retorna o valor atualizado de ingresso
+        return ingresso; 
+    }
+
+    public boolean isVisitanteMenorDeIdade(int anoDeNascimento) {
+        String dataAtual = subMenu.getData();
+        //Tratamento de data
+        int anoAtual = Integer.parseInt(dataAtual.split("/")[2]);
+        int idade = anoAtual - anoDeNascimento;
+        if (idade <= 12) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static ArrayList<RegistrosDia> vendasIngressoDia = new ArrayList<> ();
+    public static ArrayList<RegistrosDia> getVendasIngressoDia() {
+
+        return vendasIngressoDia;
+    }
+
+    private int getValorIngresso(atributosVisitante visitante) {
+        try {
+            if (isVisitanteMenorDeIdade(visitante.anoDeNascimento) == false) {
+                vendasIngressoDia.add(subMenu.getData(), this.ingresso, 100);
+                return 100;
+            } else if (isVisitanteMenorDeIdade(visitante.anoDeNascimento) == true) {
+                // Adicionar aqui, lógica com adição no Array de resultados [vendasIngressoDia]
+                return 80;
+            }
+        } catch (Exception e) {
+            System.out.println("Erro: " + e);
+        }
+    };
+
+    // Verifica se o ingresso é válido e se o visistante pode visitar uma atracao.
     public static boolean isIngressoValido(int ingresso) {
         for (Ingresso i : ingressosDoDia) {
             if (i.getIngresso() == ingresso && i.getIngressoData().equals(subMenu.getData())) {
                 return true;
             }
         }
-        return false;
+        { return false; }
     }
 
     public String getIngressoData() {// Pega a data do ingresso
@@ -51,46 +100,21 @@ public class Ingresso {
         ingressosDoDia.add(ingresso);
     }
 
-    public static ArrayList<Ingresso> getIngressos() {// Pega os ingressos do dia
+    public static ArrayList<Ingresso> getIngressosDoDia() {// Pega os ingressos do dia
         return ingressosDoDia;
     }
 
 
 
-    private Integer incrementaIngresso() { // AJUSTAR vendasIngressoDia
-        if (controleIngresso < 500 && visitante.tipo == "Adulto") {
-            ingresso = controleIngresso;
-            controleIngresso++;
-            vendasIngressoDia+= 100;
-        } else if (controleIngresso < 500 && visitante.tipo == "Crianca") {
-            ingresso = controleIngresso;
-            controleIngresso++;
-            vendasIngressoDia+= 80;
-        } else if (controleIngresso == 500){
-            System.out.println("Erro: Limite de Ingressos diários atingido.");
-        }
-        return ingresso; // Retorna o valor atualizado de ingresso
-    }
-
-    private int getValorIngresso(String tipo) {
-        try {
-            if (tipo == "Adulto") {
-                return 100;
-            } else if (tipo == "Crianca") {
-                return 80;
-            }
-        } catch (Exception e) {
-            System.out.println("Erro: " + e);
-        }
-    };
-
-    private static ArrayList<RegistrosDia> vendasIngressoDia = new ArrayList<> () implements Registros;
-    public static ArrayList<RegistrosDia> getVendasIngressoDia() {
-
-        return vendasIngressoDia;
-    }
 
     // return identificador.split("-")[-1].trim();
+
+
+
+
+
+
+
 
     public int getIngresso() {
         return ingresso;
