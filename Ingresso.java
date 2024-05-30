290bfb4c8ac50a686f1d0862dff0a09b9055ad01
Gabriel Kowaleski <142272137+K0W4@users.noreply.github.com>
2024-05-30 17:06:25 -0300

    Criação base da classe Ingresso

--- Ingresso.java
+++ Ingresso.java
@@ -1,3 +1,20 @@
-public class Ingresso {
-    
+//Imports.
+import java.util.ArrayList;
+
+//Criação da classe Ingresso.
+public class Ingresso
+{
+    //Criação das variáveis.
+    private int cont, dia;
+    private ArrayList<Visitante> ingressos;
+    private ArrayList<ArrayList> dias;
+
+    //Construtor.
+    public Ingresso()
+    {
+        this.cont = cont;
+        this.dia = dia;
+        ingressos = new ArrayList<>();
+        dias = new ArrayList<>();
+    }
 }
