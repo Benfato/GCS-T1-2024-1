7561d7d64ad144895f86c368aad609f132298d08
LeoBertoletti <Leonardo.b@edu.pucrs.br>
2024-06-05 22:42:15 -0300

    Ingresso Patch
    Co-authored-by: GabriellaLSchmidt <GabriellaLSchmidt@users.noreply.github.com>

--- Ingresso.java
+++ Ingresso.java
@@ -1,57 +1,60 @@
+
 //Imports.
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 
 //Criação da classe Ingresso.
-public class Ingresso
-{
-    //Criação das variáveis.
-    private int cont, dia;
-    private ArrayList<Visitante> ingressos;
-    private ArrayList<ArrayList> dias;
+public class Ingresso {
+    // Criação das variáveis.
+    private int contadorIngressos = -1;
+    private HashMap<Integer, Visitante> listaVizitantes;
+    private HashMap<LocalDate, HashMap<Integer, Visitante>> historico;
 
-    //Construtor.
-    public Ingresso()
-    {
-        this.cont = cont;
-        this.dia = dia;
-        ingressos = new ArrayList<>();
-        dias = new ArrayList<>();
+    // Construtor.
+    private Ingresso(Visitante visitante) {
+        contadorIngressos = this.contadorIngressos++;
+        LocalDate data = LocalDate.now();
+        this.listaVizitantes.put(contadorIngressos, visitante);
     }
 
-    //Método para consultar se um visitante possui um ingresso.
-    public boolean consultaIngresso(Visitante v1)
-    {
-        for (Visitante v2 : ingressos)
-        {
-            if (v1.getCpf() == v2.getCpf())
-            {
+    // Método para consultar se um visitante possui um ingresso.
+    public boolean consultaIngresso(Visitante visitante) {
+        Collection<Visitante> lista = this.listaVizitantes.values();
+        for (Visitante v2 : lista) {
+            if (visitante.getCpf() == v2.getCpf()) {
                 return true;
             }
         }
         return false;
     }
 
-    //Método para cadastrar um ingresso a um visitante.
-    public boolean cadastraIngresso(Visitante v)
-    {
-        if (cont < 500)
-        {
-            if (!consultaIngresso(v))
-            {
-                ingressos.add(v);
-                cont++;
+    // Método para cadastrar um ingresso a um visitante.
+    public boolean cadastraIngresso(Visitante visitante) {
+        if (contadorIngressos < 500) {
+            if (!consultaIngresso(visitante)) {
+                new Ingresso(visitante);
                 return true;
             }
         }
+        encerraDia();
         return false;
     }
 
-    //Método para cadastrar um dia.
-    public void cadastraDia(ArrayList<Visitante> ingressos)
-    {
-        cont = 0;
-        dia ++;
-
-        dias.add(ingressos);
+    public Visitante getVisitanteByIngresso(int ingresso) {
+        return listaVizitantes.get(ingresso);
     }
+
+    public ArrayList<Visitante> getVisitantesByData(LocalDate data) {
+        return new ArrayList<>(historico.get(data).values());
+    }
+
+    // Método para cadastrar um dia.
+    private void encerraDia() {
+        this.contadorIngressos = 0;
+        historico.put(LocalDate.now(), listaVizitantes);
+        this.listaVizitantes.clear();
+    }
+
 }
