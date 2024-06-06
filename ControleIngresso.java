46ebfb4b9a13ed978a67a22c4ec4fe9bc24e2f13
LeoBertoletti <Leonardo.b@edu.pucrs.br>
2024-06-06 00:54:59 -0300

    Alterações: Ingressos e Atracoes Criados: ControleIngresso and ControleAtracao

--- ControleIngresso.java
+++ ControleIngresso.java
@@ -23,6 +23,20 @@ public class ControleIngresso {
         return listaVizitantes.get(ingresso);
     }
 
+    public int getQuantidadeIngressos() {
+        return listaVizitantes.size();
+    }
+
+    public int getIngressoByVisitante(Visitante visitante) {
+        Collection<Visitante> lista = this.listaVizitantes.values();
+        for (int i = 0; i < lista.size(); i++) {
+            if (visitante.equals(lista.toArray()[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public ArrayList<Visitante> getVisitantesByData(LocalDate data) {
         return new ArrayList<>(historico.get(data).values());
     }
@@ -35,6 +49,26 @@ public class ControleIngresso {
             }
         }
         return false;
+    }
+
+    // falta a criação do método que retorne o tipo ou o valor do ingresso para
+    // verificação.
+    public int getFaturamentoPeriodo(LocalDate inicio, LocalDate fim) {
+        int faturamento = 0;
+        for (LocalDate data = inicio; data.isBefore(fim); data = data.plusDays(1)) {
+            if (historico.containsKey(data)) {
+                Collection<Visitante> lista = this.historico.get(data).values();
+                for (Visitante v : lista) {
+                    if (v.getTipoIngresso() == TipoIngresso.INTEIRO) {
+                        faturamento += 100;
+                    } else if (v.getTipoIngresso() == TipoIngresso.INFANTIL) {
+                        faturamento += 80;
+                    }
+                }
+            }
+
+        }
+        return faturamento;
     }
 
     public void encerraDia() {
--- Ingresso.java
+++ Ingresso.java
@@ -1,22 +1,18 @@
-
-//Imports.
 import java.time.LocalDate;
 
-//Criação da classe Ingresso.
 public class Ingresso {
-    TRP trp = new TRP();
+    ControleIngresso controladorIngresso = new ControleIngresso();
     private int contadorIngressos = 0;
 
-    // Construtor.
     public Ingresso(Visitante visitante) {
         if (contadorIngressos < 500) {
             int ingresso = this.contadorIngressos;
             LocalDate data = LocalDate.now();
-            trp.addVisitante(contadorIngressos, visitante);
+            controladorIngresso.addVisitante(contadorIngressos, visitante);
             this.contadorIngressos++;
             System.out.println("Ingresso cadastrado com sucesso!");
         } else {
-            trp.encerraDia();
+            controladorIngresso.encerraDia();
             contadorIngressos = 0;
             System.out.println("Limite de ingressos atingido, encerrando o dia.");
         }
