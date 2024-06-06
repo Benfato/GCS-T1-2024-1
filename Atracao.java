25e828b0642cce0af9a61dfc59984c91d82c91ad
MarceloBenfato <mbenfato@outlook.com>
2024-06-06 19:27:59 -0300

    Atualizando Atrações

--- Atracao.java
+++ Atracao.java
@@ -1,7 +1,16 @@
 import java.util.ArrayList;
 
-public class Atracao {
+class Atracao {
     private String nome;
     private ArrayList<Visitante> visitante;
+
+    public Atracao(String nome) {
+        this.nome = nome;
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
 
 }
--- mainMenu.java
+++ mainMenu.java
@@ -2,6 +2,27 @@ import java.util.Scanner;
 
 public class mainMenu {
     public static void main(String[] args) {
+
+
+        Atracoes atracoes = new Atracoes();
+        atracoes.cadastrarAtracao("Montanha Russa");
+        atracoes.cadastrarAtracao("Roda Gigante");
+        atracoes.cadastrarAtracao("Casa de Espelhos");
+        atracoes.cadastrarAtracao("Carrossel");
+        atracoes.cadastrarAtracao("Barco Viking");
+        atracoes.cadastrarAtracao("Carrinho de Bate-Bate");
+        atracoes.cadastrarAtracao("Trem Fantasma");
+        atracoes.cadastrarAtracao("Cinema 4D");
+        atracoes.cadastrarAtracao("Tiro ao Alvo");
+        atracoes.cadastrarAtracao("Torre de Queda Livre");
+        atracoes.cadastrarAtracao("Torre de Escalada");
+        atracoes.cadastrarAtracao("Jogo de Laser Tag");
+        atracoes.cadastrarAtracao("Piscina de Bolinhas");
+        atracoes.cadastrarAtracao("Parede de Escalada");
+        atracoes.cadastrarAtracao("Pista de Kart");
+
+
+
         Scanner sc = new Scanner(System.in);
         System.out.println("\t====== PARK TUPÃ - A 49 anos levando a diversão até você ======");
         System.out.println("\t\t====== Sistema de gerenciamento de visitantes ======");
@@ -10,7 +31,8 @@ public class mainMenu {
         System.out.println("3 - Localizar visitante");
         System.out.println("4 - Consultar faturamento");
         System.out.println("5 - Consultar atrações");
-        System.out.println("6 - Encerrar sistema");
+        System.out.println("6 - Encerrar dia");
+        System.out.println("7 - Encerrar sistema");
 
         int selecao = sc.nextInt();
         switch (selecao){
@@ -24,7 +46,10 @@ public class mainMenu {
                 //todo método consultar faturamento
             case 5:
                 //todo método consultar atrações
+                atracoes.exibirAtracoes();
             case 6:
+                //todo método encerrar dia
+            case 7:
                 sc.close();
                 System.exit(0);
         }
