8b937818e337c7e816eeb9791d1f12ba9500c2d3
ThiagoDefini <thiagod.scherer@hotmail.com>
2024-06-07 05:16:30 -0300

    Alteração feita no nome da variavel visitanteDaAtracao

--- Atracao.java
+++ Atracao.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 public class Atracao {
     String nome;
     
-    public ArrayList<VisitanteDaAtracao> visitantes;
+    public ArrayList<VisitanteDaAtracao> visitantesDaAtracao;
 
     public Atracao(String nome){
         this.nome = nome;
@@ -12,7 +12,7 @@ public class Atracao {
 
     public void adicionarVisitante(Visitante visitante, LocalDate data){
         VisitanteDaAtracao v = new VisitanteDaAtracao(visitante, data);
-        this.visitantes.add(v);
+        this.visitantesDaAtracao.add(v);
     }
 
     public String getNome() {
