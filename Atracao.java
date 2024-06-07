00ed28e18def4526c5da9e386743bfe5c5b120f6
MayraBodin <mayrabordindeabreu@gmail.com>
2024-06-07 13:23:42 -0300

    Adicionando a funcionalidade de capacidade maxima

--- Atracao.java
+++ Atracao.java
@@ -1,10 +1,46 @@
 import java.util.ArrayList;
-public class Atracao {
-    String nome;
-    
-    private ArrayList<Visitante> visitante;
 
-    public void adicionarVisitante(Visitante visitante){
-        this.visitante.add(visitante);
+public class Atracao {
+    private String nome;
+    private int capacidadeMaxima;
+    private ArrayList<Visitante> visitantes;
+
+    public Atracao(String nome, int capacidadeMaxima) {
+        this.nome = nome;
+        this.capacidadeMaxima = capacidadeMaxima;
+        this.visitantes = new ArrayList<>();
+    }
+
+    public void adicionarVisitante(Visitante visitante) {
+        if (this.visitantes.size() < this.capacidadeMaxima) {
+            this.visitantes.add(visitante);
+        } else {
+            System.out.println("Desculpe, a capacidade máxima desta atração foi alcançada. Não é possível adicionar mais visitantes.");
+        }
+    }
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCapacidadeMaxima() {
+        return capacidadeMaxima;
+    }
+
+    public void setCapacidadeMaxima(int capacidadeMaxima) {
+        this.capacidadeMaxima = capacidadeMaxima;
+    }
+
+    public ArrayList<Visitante> getVisitantes() {
+        return visitantes;
+    }
+
+    public void setVisitantes(ArrayList<Visitante> visitantes) {
+        this.visitantes = visitantes;
     }
 }
