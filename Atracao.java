1608b09db101da050b93169d0b31ecc18e301049
MayraBodin <mayrabordindeabreu@gmail.com>
2024-06-07 16:15:51 -0300

    Adicionando Funcionalide Capacidade Maxima na branch de desenvolvimento

--- Atracao.java
+++ Atracao.java
@@ -1,22 +1,46 @@
 import java.util.ArrayList;
-import java.time.LocalDate;
 
 public class Atracao {
-    String nome;
-    
-    public ArrayList<VisitanteDaAtracao> visitantesDaAtracao;
+    private String nome;
+    private int capacidadeMaxima;
+    private ArrayList<Visitante> visitantes;
 
-    public Atracao(String nome){
+    public Atracao(String nome, int capacidadeMaxima) {
         this.nome = nome;
+        this.capacidadeMaxima = capacidadeMaxima;
+        this.visitantes = new ArrayList<>();
     }
 
-    public void adicionarVisitante(Visitante visitante, LocalDate data){
-        VisitanteDaAtracao v = new VisitanteDaAtracao(visitante, data);
-        this.visitantesDaAtracao.add(v);
+    public void adicionarVisitante(Visitante visitante) {
+        if (this.visitantes.size() < this.capacidadeMaxima) {
+            this.visitantes.add(visitante);
+        } else {
+            System.out.println("Desculpe, a capacidade máxima desta atração foi alcançada. Não é possível adicionar mais visitantes.");
+        }
     }
 
+    // Getters e Setters
     public String getNome() {
         return nome;
     }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCapacidadeMaxima() {
+        return capacidadeMaxima;
+    }
+
+    public void setCapacidadeMaxima(int capacidadeMaxima) {
+        this.capacidadeMaxima = capacidadeMaxima;
+    }
+
+    public ArrayList<Visitante> getVisitantes() {
+        return visitantes;
+    }
+
+    public void setVisitantes(ArrayList<Visitante> visitantes) {
+        this.visitantes = visitantes;
+    }
 }
